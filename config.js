6bdcc395ab97069b316e729fab0b3d094068e69a
Thanh Long <tranthanhlong.q9@gmail.com>
2021-07-04 18:40:52 +0700

    up 3

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 const CONFIG = {
-    introTitle: 'Bạn gì ấy ơiii ^^',    
-    introDesc: `Mình nói bạn đóoo.. 
-    Sau đây mình có một bất ngờ dành riêng cho bạn..
+    introTitle: 'Cậu gì ấy ơiii ^^',    
+    introDesc: `Tớ nói cậu đóoo .-.
+    Sau đây tớ có một bất ngờ dành riêng cho cậu, 
     Trust me <3 `,
     //Trái đất vốn lạ thường
     //Mà sao em cứ đi nhầm đường
     //Lạc vào tim anh lẻ loi
     //Đằng sau chữ yêu đây là thương
     btnIntro: 'Bấm vào đây nhé ^^',
-    title: 'Chào mừng bạn đã đến với bất ngờ của mình 😙',
+    title: 'Chào mừng cậu đã đến với thế giới của tớ 😙',
     //Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙
-    desc: 'Phải chăng em đã thích anh rồi đúng không <3',
+    desc: 'Phải chăng cậu thích mình đúng không <3',
     //Bạn có muốn làm cả thế giới của mình không<3
     //Phải chăng em đã say ngay từ lúc thấy nụ cười ấy 
-    btnYes: 'Thích anh lắm á <33',
-    btnNo: 'Mình chỉ lỡ bấm thôi :))',
-    question: 'Việt Nam có hơn trăm triệu người, điều gì đã khiến em thích anh <3',
+    btnYes: 'Thích cậu lắm á <33',
+    btnNo: 'Mình chỉ bấm nhầm thôi :))',
+    question: 'Điều gì đã khiến cậu thích tớ <3',
     //Việt Nam có hơn trăm triệu người, điều gì đã khiến em thích anh<3
     //Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh<3
-    btnReply: 'Gửi cho anh đi <3',
-    reply: 'Bởi vì anh thật đẹp trai, chuẩn gu emm <33',
-    mess: 'Ỏww..🥰 Mời em đến với thế giới của anh.. Sao hai ta không thử tìm hiểu nhau 😘😘',
+    btnReply: 'Gửi cho tớ nha <3',
+    reply: 'Bởi vì cậu thật đẹp trai, chuẩn gu tớ <33',
+    mess: 'Ỏww..🥰 Mời cậu đến với thế giới của tớ.. Sao hai ta không thử ib tìm hiểu nhau 😘😘',
     messDesc: 'Thử like 1 bài viết của mình và mình sẽ ib cho bạn.. ❤',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/iamlong.limited/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/iamlong.limited/' //link mess của các bạn.
 }
