93ff92a5516ab42a90bb7d240402d40dd50a6cc3
Thanh Long <tranthanhlong.q9@gmail.com>
2021-07-04 13:56:14 +0700

    fix

--- config.js
+++ config.js
@@ -1,19 +1,27 @@
 const CONFIG = {
-    introTitle: 'Em aaaơi!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Chào Anh',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhá công chúa.',
+    introTitle: 'Bạn gì ấy ơiii ^^',    
+    introDesc: `Mình nói bạn đóoo.., 
+    Sau đây mình có một bất ngờ dành riêng cho bạn..,
+    Trust me <3 `,
+    //Trái đất vốn lạ thường
+    //Mà sao em cứ đi nhầm đường
+    //Lạc vào tim anh lẻ loi
+    //Đằng sau chữ yêu đây là thương
+    btnIntro: 'Bấm vào đây nhé ^^',
+    title: 'Chào mừng bạn đã đến với bất ngờ của mình, Hãy cùng mình nghe nhạc và trả lời câu hỏi nhá 😙',
+    //Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙
+    desc: 'Phải chăng em thích anh rồi đúng không <3',
+    //Bạn có muốn làm cả thế giới của mình không<3
+    //Phải chăng em đã say ngay từ lúc thấy nụ cười ấy 
+    btnYes: 'Thích anh lắm á <33',
+    btnNo: 'Không, Mình chỉ lỡ bấm vào thôi :))',
+    question: 'Việt Nam có hơn trăm triệu người, điều gì đã khiến em thích anh <3',
+    //Việt Nam có hơn trăm triệu người, điều gì đã khiến em thích anh<3
+    //Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh<3
+    btnReply: 'Gửi cho anh đi <3',
+    reply: 'Bởi vì anh thật đẹp trai, chuẩn gu emm <33',
+    mess: 'Ỏww..🥰 Mời em đến với thế giới của anh.. Sao hai ta không thử tìm hiểu nhau 😘😘',
+    messDesc: 'Thử like 1 bài viết của mình và mình sẽ ib cho bạn.. ❤',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/iamlong.limited/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
